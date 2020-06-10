59a3d92dec9a6397730dd476d89a7bba3ce274df
David Delahaye <delahaye3@hotmail.co.uk>
2020-06-10 22:24:30 +0100

    initial Commit

--- Server/db.js
+++ Server/db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'gov15bees',
-    host: 'localhost',
-    port: 5433,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     database: 'perntodo'
 });
 
