6d7d220f493424770c7e1ed2a960f3c614cd2e6d
David Delahaye <delahaye3@hotmail.co.uk>
2020-06-12 06:58:03 +0100

    heroku change

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
-const Pool = require('./node_modules/pg').Pool;
+const {Client} = require('./node_modules/pg').Pool;
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: 'perntodo'
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
 });
 
-module.exports = pool;
+client.connect();
+module.exports = client;
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 require('dotenv').config()
 const PORT = (process.env.PORT || 5000);
 const cors = require('cors');
-const pool = require('./db');
+const client = require('./db');
 
 //MiddleWare
 app.use(cors());
@@ -14,7 +14,7 @@ app.use(express.json());
 //get all todos
 app.get("/todos", async(req,res) => {
     try{
-        const allTodos = await pool.query("SELECT * FROM todo");
+        const allTodos = await client.query("SELECT * FROM todo");
         res.json(allTodos.rows);
     }catch(err){
         console.error(err.message);
@@ -25,7 +25,7 @@ app.get("/todos", async(req,res) => {
 app.post("/todos", async(req,res) => {
     try {
         const {description} = req.body;
-        const newTodo = await pool.query(
+        const newTodo = await client.query(
             "INSERT INTO todo (description) VALUES($1) RETURNING*",
              [description]);
         res.json(newTodo.rows[0]);
@@ -39,7 +39,7 @@ app.get("/todos/:id", async(req,res) => {
     try {
         const {id} = req.params;
         console.log(id);
-        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+        const todo = await client.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
         res.json(todo.rows[0]);
     } catch (err) {
         console.error(err.message);
@@ -51,7 +51,7 @@ app.put("/todos/:id", async(req,res) => {
     try {
         const {id} = req.params;
         const {description} = req.body;
-        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
+        const updateTodo = await client.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
         res.json("Todo was Updated")
     } catch (err) {
         console.error(err.message);
@@ -62,7 +62,7 @@ app.put("/todos/:id", async(req,res) => {
 app.delete("/todos/:id", async(req,res) =>{
 try {
     const {id} = req.params;
-    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+    const deleteTodo = await client.query("DELETE FROM todo WHERE todo_id = $1", [id]);
     res.json("Todo was deleted")
 } catch (err) {
     console.error(err.message);
